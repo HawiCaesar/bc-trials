d813b9a8722d4232cac47492f4d6a439d98d7eaa
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-13 22:35:30 +0300

    added contact-us, contact-us route to views.py, minor changes to base and about

--- app/views.py
+++ app/views.py
@@ -10,6 +10,10 @@ def index():
 def login():
 	return render_template('login.html')
 
+@app.route('/contact-us')
+def contact():
+	return render_template('contact-us.html')
+
 @app.route('/about')
 def about():
     return render_template("about.html")
